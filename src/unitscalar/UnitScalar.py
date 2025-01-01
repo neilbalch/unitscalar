620c5d88485064901eea10686176d718729f46de
Neil Balch <neilbalch@gmail.com>
2024-12-31 20:54:41 -0800

    Fix tests

--- src/unitscalar/UnitScalar.py
+++ src/unitscalar/UnitScalar.py
@@ -5,6 +5,7 @@ import numbers
 import math
 from custom_literals import literals, lie, rename
 
+
 @literals(float, int)
 class UnitScalar(lie(float)):
     VALID_UNITS = {
@@ -17,7 +18,7 @@ class UnitScalar(lie(float)):
         "in": ("m", "", 0.0254),
         "L": ("m3", "", 1e-3),
         "Hz": ("1", "s", 1.0),
-        "rpm": ("1", "s", 1/60),
+        "rpm": ("1", "s", 1 / 60),
         "g": ("kg", "", 1e-3),
         "lbm": ("kg", "", 0.45359237),
         "J": ("kg m2", "s2", 1.0),
@@ -27,64 +28,68 @@ class UnitScalar(lie(float)):
         # "mol": ("", "", 6.02214076e23),
         "mol": ("mol", "", 1.0),
         "N": ("kg m", "s2", 1.0),
-        "lbf": ("kg m", "s2", 9.80665*0.45359237),
+        "lbf": ("kg m", "s2", 9.80665 * 0.45359237),
         "Pa": ("N", "m2", 1.0),
-        "hPa": ("N", "m2", 1e2),    # Hectopascal
+        "hPa": ("N", "m2", 1e2),  # Hectopascal
         "bar": ("N", "m2", 1e5),
-        "atm": ("N", "m2", 101325.0),   # Atmosphere
-        "psi": ("N", "m2", 9.80665*0.45359237/(0.0254**2)),
+        "atm": ("N", "m2", 101325.0),  # Atmosphere
+        "psi": ("N", "m2", 9.80665 * 0.45359237 / (0.0254**2)),
         "W": ("J", "s", 1.0),
-        "Ah": ("C", "", 3600.0),    # Amp-Hour
+        "Ah": ("C", "", 3600.0),  # Amp-Hour
         "A": ("C", "s", 1.0),
         "V": ("J", "C", 1.0),
         "ohm": ("V", "A", 1.0),
-        "T": ("V s", "m2", 1.0),    # Tesla
-        "F": ("C", "V", 1.0),       # Farad
+        "T": ("V s", "m2", 1.0),  # Tesla
+        "F": ("C", "V", 1.0),  # Farad
         "H": ("m2 kg", "C2", 1.0),  # Henry
     }
 
     VALID_PREFIXES = {
-        "f": 1e-15, # femto
-        "p": 1e-12, # pico
-        "n": 1e-9, # nano
-        "u": 1e-6, # micro
-        "m": 1e-3, # milli
-        "k": 1e3, # kilo
-        "M": 1e6, # mega
-        "G": 1e9, # giga
-        "T": 1e12, # tera
+        "f": 1e-15,  # femto
+        "p": 1e-12,  # pico
+        "n": 1e-9,  # nano
+        "u": 1e-6,  # micro
+        "m": 1e-3,  # milli
+        "k": 1e3,  # kilo
+        "M": 1e6,  # mega
+        "G": 1e9,  # giga
+        "T": 1e12,  # tera
     }
 
     @dataclass
-    class SimpleUnit: # Fundamental data type stored in the num_unit, den_unit lists
-        unit: str # Must be a member of UnitScalar.VALID_UNITS
+    class SimpleUnit:  # Fundamental data type stored in the num_unit, den_unit lists
+        unit: str  # Must be a member of UnitScalar.VALID_UNITS
         exp: int
 
     @staticmethod
-    def _reduce_units(num_units: list[UnitScalar.SimpleUnit], den_units: list[UnitScalar.SimpleUnit]) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit]]:
-      i=0
-      while i < len(num_units):
-          j=0
-          while j < len(den_units):
-              if num_units[i].unit == den_units[j].unit:
-                  if num_units[i].exp > den_units[j].exp:
-                      num_units[i].exp = num_units[i].exp - den_units[j].exp
-                      del den_units[j]
-                  elif num_units[i].exp == den_units[j].exp:
-                      del den_units[j]
-                      del num_units[i]
-                      i -= 1
-                  else:
-                      den_units[j].exp = den_units[j].exp - num_units[i].exp
-                      del num_units[i]
-                      i -= 1
-              j += 1
-          i += 1
-      return num_units, den_units
+    def _reduce_units(
+        num_units: list[UnitScalar.SimpleUnit], den_units: list[UnitScalar.SimpleUnit]
+    ) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit]]:
+        i = 0
+        while i < len(num_units):
+            j = 0
+            while j < len(den_units):
+                if num_units[i].unit == den_units[j].unit:
+                    if num_units[i].exp > den_units[j].exp:
+                        num_units[i].exp = num_units[i].exp - den_units[j].exp
+                        del den_units[j]
+                    elif num_units[i].exp == den_units[j].exp:
+                        del den_units[j]
+                        del num_units[i]
+                        i -= 1
+                    else:
+                        den_units[j].exp = den_units[j].exp - num_units[i].exp
+                        del num_units[i]
+                        i -= 1
+                j += 1
+            i += 1
+        return num_units, den_units
 
     # Merge lists of SimpleUnit, taking care to not duplicate entries
     @staticmethod
-    def _merge_lists(la: list[UnitScalar.SimpleUnit], lb: list[UnitScalar.SimpleUnit]) -> list[UnitScalar.SimpleUnit]:
+    def _merge_lists(
+        la: list[UnitScalar.SimpleUnit], lb: list[UnitScalar.SimpleUnit]
+    ) -> list[UnitScalar.SimpleUnit]:
         out = copy.deepcopy(la)
         for x in lb:
             located = False
@@ -100,7 +105,9 @@ class UnitScalar(lie(float)):
     # Parse complicated unit string, e.g. "kg mm / ms2", into a list of base SI units
     # for the numerator and denominator, and a multiplication factor combining all
     # unit prefixes together
-    def _parse_units(unit_str: str) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit], float]:
+    def _parse_units(
+        unit_str: str,
+    ) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit], float]:
         split = unit_str.split("/")
         num_str = split[0] if len(split) > 0 else ""
         den_str = split[1] if len(split) > 1 else ""
@@ -114,7 +121,9 @@ class UnitScalar(lie(float)):
         # Parse (potentially complex) unit string, e.g. "uJ3", into a list of
         # SimpleUnits for the numerator and denominator, and a multiple to describe
         # the prefix and conversion to SI base units
-        def identify_unit(unit_str: str) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit], float]:
+        def identify_unit(
+            unit_str: str,
+        ) -> tuple[list[UnitScalar.SimpleUnit], list[UnitScalar.SimpleUnit], float]:
             # Steps:
             # 1. Break str into prefix, unit (member of VALID_UNITS), and an exponent
             # 2. Decide whether unit is a base unit (one of SI base units)
@@ -127,7 +136,7 @@ class UnitScalar(lie(float)):
 
             # Find first number in the string (exponent). Mark None if does not exist
             # https://stackoverflow.com/a/22446407/3339274
-            for (idx_first_num, c) in enumerate(unit_str):
+            for idx_first_num, c in enumerate(unit_str):
                 if c.isdigit():
                     break
             else:
@@ -156,7 +165,9 @@ class UnitScalar(lie(float)):
                 exp = 1
 
             # Is the unit already a single SI unit? (i.e. not composed of multiple units)
-            if (unit[0] == "" or unit[1] == "") and ((not " " in unit[0]) and (not " " in unit[1])):
+            if (unit[0] == "" or unit[1] == "") and (
+                (not " " in unit[0]) and (not " " in unit[1])
+            ):
                 if unit[1] == "":
                     num_units.append(UnitScalar.SimpleUnit(unit[0], exp))
                 else:
@@ -164,7 +175,9 @@ class UnitScalar(lie(float)):
             # Recurse on the numerator and denominator until the unit string is a single SI unit
             else:
                 # Feed the unit back into parse_units to have it broken down into SI units
-                num_units, den_units, mult_inner = UnitScalar._parse_units(f"{unit[0]} / {unit[1]}")
+                num_units, den_units, mult_inner = UnitScalar._parse_units(
+                    f"{unit[0]} / {unit[1]}"
+                )
                 # Apply outer exponent to all inner terms
                 for unit in num_units:
                     unit.exp *= exp
@@ -172,7 +185,7 @@ class UnitScalar(lie(float)):
                     unit.exp *= exp
                 mult *= mult_inner
 
-            mult = mult ** exp
+            mult = mult**exp
 
             return num_units, den_units, mult
 
@@ -205,14 +218,16 @@ class UnitScalar(lie(float)):
 
     def __init__(self, num: numbers.Real, unit: str):
         self.num_unit, self.den_unit, units_mult = UnitScalar._parse_units(unit)
-        self.num_unit, self.den_unit = UnitScalar._reduce_units(self.num_unit, self.den_unit)
+        self.num_unit, self.den_unit = UnitScalar._reduce_units(
+            self.num_unit, self.den_unit
+        )
         self.num = num * units_mult
 
     def __str__(self) -> str:
         unit_str = ""
         for unit, i in zip(self.num_unit, range(len(self.num_unit))):
             unit_str += f"{unit.unit}{unit.exp if unit.exp > 1 else ''}"
-            if i+1 != len(self.num_unit):
+            if i + 1 != len(self.num_unit):
                 unit_str += " "
         if len(self.den_unit) > 0:
             if len(self.num_unit) > 0:
@@ -221,10 +236,14 @@ class UnitScalar(lie(float)):
                 unit_str += "1/"
         for unit, i in zip(self.den_unit, range(len(self.den_unit))):
             unit_str += f"{unit.unit}{unit.exp if unit.exp > 1 else ''}"
-            if i+1 != len(self.den_unit):
+            if i + 1 != len(self.den_unit):
                 unit_str += " "
 
-        return f"{(self.num):.2f} {unit_str}" if abs(self.num) > 1e-2 else f"{(self.num):.2E} {unit_str}"
+        return (
+            f"{(self.num):.2f} {unit_str}"
+            if abs(self.num) > 1e-2
+            else f"{(self.num):.2E} {unit_str}"
+        )
 
     # Returns in base (mKgs) units
     def __float__(self) -> float:
@@ -238,8 +257,12 @@ class UnitScalar(lie(float)):
     def units_agree(self, other: UnitScalar | str) -> bool:
         if isinstance(other, UnitScalar):
             # Substitute and reduce
-            self_num_unit, self_den_unit = UnitScalar._reduce_units(self.num_unit, self.den_unit)
-            oth_num_unit, oth_den_unit = UnitScalar._reduce_units(other.num_unit, other.den_unit)
+            self_num_unit, self_den_unit = UnitScalar._reduce_units(
+                self.num_unit, self.den_unit
+            )
+            oth_num_unit, oth_den_unit = UnitScalar._reduce_units(
+                other.num_unit, other.den_unit
+            )
 
             # Sort by unit string
             self_num_sort = sorted(self_num_unit, key=lambda x: x.unit)
@@ -335,7 +358,9 @@ class UnitScalar(lie(float)):
             new = UnitScalar(self.num * other.num, "")
             new.num_unit = UnitScalar._merge_lists(self.num_unit, other.num_unit)
             new.den_unit = UnitScalar._merge_lists(self.den_unit, other.den_unit)
-            new.num_unit, new.den_unit = UnitScalar._reduce_units(new.num_unit, new.den_unit)
+            new.num_unit, new.den_unit = UnitScalar._reduce_units(
+                new.num_unit, new.den_unit
+            )
             return new
         elif isinstance(other, numbers.Real):
             new = UnitScalar(self.num * other, "")
@@ -350,7 +375,9 @@ class UnitScalar(lie(float)):
             new = UnitScalar(self.num / other.num, "")
             new.num_unit = UnitScalar._merge_lists(self.num_unit, other.den_unit)
             new.den_unit = UnitScalar._merge_lists(self.den_unit, other.num_unit)
-            new.num_unit, new.den_unit = UnitScalar._reduce_units(new.num_unit, new.den_unit)
+            new.num_unit, new.den_unit = UnitScalar._reduce_units(
+                new.num_unit, new.den_unit
+            )
             return new
         elif isinstance(other, numbers.Real):
             new = UnitScalar(self.num / other, "")
@@ -365,7 +392,9 @@ class UnitScalar(lie(float)):
             new = UnitScalar(other.num / self.num, "")
             new.num_unit = UnitScalar._merge_lists(self.den_unit, other.num_unit)
             new.den_unit = UnitScalar._merge_lists(self.num_unit, other.den_unit)
-            new.num_unit, new.den_unit = UnitScalar._reduce_units(new.num_unit, new.den_unit)
+            new.num_unit, new.den_unit = UnitScalar._reduce_units(
+                new.num_unit, new.den_unit
+            )
             return new
         elif isinstance(other, numbers.Real):
             new = UnitScalar(other / self.num, "")
@@ -376,7 +405,7 @@ class UnitScalar(lie(float)):
             return NotImplemented
 
     def __pow__(self, power: numbers.Integral) -> UnitScalar:
-        new = UnitScalar(self.num ** power, "")
+        new = UnitScalar(self.num**power, "")
         new.num_unit = copy.deepcopy(self.num_unit)
         new.den_unit = copy.deepcopy(self.den_unit)
         for i in range(len(new.num_unit)):
@@ -394,18 +423,23 @@ class UnitScalar(lie(float)):
     @rename("x")
     def to_unitless(self: float | int):
         return UnitScalar(self, "")
+
     @rename("gMM")
     def to_gram_molar_mass(self: float | int):
         return UnitScalar(self, "g/mol")
+
     @rename("inch")
     def to_inches(self: float | int):
         return UnitScalar(self, "in")
+
     # @rename("psi") # Doesn't work, for some reason?
     def psi(self: float | int):
         return UnitScalar(self, "psi")
+
     @rename("lbf")
     def to_psi(self: float | int):
         return UnitScalar(self, "lbf")
+
     @rename("K")
     def to_kelvin(self: float | int):
         return UnitScalar(self, "K")
